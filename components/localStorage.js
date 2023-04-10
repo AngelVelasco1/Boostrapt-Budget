827929afa020ae0ffbb00741aa9cb009600131fe
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-04-10 16:32:23 -0500

    feat: :sparkles: Implementacion del localStorage a la tabla

--- components/localStorage.js
+++ components/localStorage.js
@@ -3,58 +3,53 @@ export default {
         /* Variables */
         let income = [];
         let outgoing = [];
-        const wk = new Worker("./storage/wkLocalStorage.js", { type: "module" });
+        const wk = new Worker("storage/wkLocalStorage.js", { type: "module" });
         /* Call Functions */
-        setLocalStorage();
         loadLocalStorage();
         /* Event Update Table*/
         let submit = document.querySelector("#submit");
-        submit.addEventListener("click", () => {
+        submit.addEventListener("click", (e) => {
 
-            let select = document.querySelector("#select");
+            let select = document.querySelector("#select").value;
+            let description = document.querySelector("#description").value;
             let price = document.querySelector("#price").value;
-            if (isNaN(price)) {
-                return;
+
+            if (!isNaN(parseInt(price)) && price > 0) {
+                wk.postMessage({ select: select, description: description, price: parseInt(price) })
             }
-
-            else if (price <= 0) {
-                return;
+            else {
+                ("nada");
             }
+        });
+        
+        wk.onmessage = ((e) => {
+            let select = e.data.select;
+            let data = e.data.data;
+            let template = e.data.template;
 
-            if (select.value === "plus") {
-                let tableIncome = document.querySelector("#tableIncome tbody");
-                let description = document.querySelector("#type").value;
+            if (select === "load") {
+                let Itemplate = e.data.data.Itemplate;
+                let Otemplate = e.data.data.Otemplate;
 
-                let newHead = document.createElement("th")
-                let newRow = document.createElement("tr");
-                let newCell1 = document.createElement("td");
-                let newCell2 = document.createElement("td");
-
-                newCell1.textContent = description;
-                newCell2.textContent = `$${price}`;
-
-                newRow.appendChild(newCell1);
-                newRow.appendChild(newCell2);
-                tableIncome.appendChild(newRow);
+                updateTable("tableIncome", Itemplate);
+                updateTable("tableOutgoing", Otemplate);
             }
-
-            if (select.value === "minus") {
-                let tableOutgoing = document.querySelector("#tableOutgoing tbody");
-                let description = document.querySelector("#type").value;
-
-                let newRow = document.createElement("tr");
-                let newCell1 = document.createElement("td");
-                let newCell2 = document.createElement("td");
-
-                newCell1.textContent = description;
-                newCell2.textContent = `-$${price}`;
-
-                newRow.appendChild(newCell1);
-                newRow.appendChild(newCell2);
-                tableOutgoing.appendChild(newRow);
+            if (select === "incomee") {
+                income = data;
+                updateTable("tableIncome", template);
+            }else if (select === "outgoingg") {
+                outgoing = data;
+                updateTable("tableOutgoing", template);
             }
-        })
+        });
+
+
         /* Functions */
+        function updateTable(tableId, template) {
+            document.querySelector(`#${tableId} tbody`).innerHTML = template;
+            setLocalStorage();
+        }
+
         function setLocalStorage() {
             localStorage.setItem("incomeList", JSON.stringify(income));
             localStorage.setItem("outgoingList", JSON.stringify(outgoing));
@@ -63,7 +58,39 @@ export default {
         function loadLocalStorage() {
             income = JSON.parse(localStorage.getItem("incomeList")) || [];
             outgoing = JSON.parse(localStorage.getItem("outgoingList")) || [];
-            wk.postMessage({ type: "load", data: { income: income, outgoing: outgoing } })
+            wk.postMessage({ select: "load", data: { income: income, outgoing: outgoing } })
         }
     }
 } 
+
+            /*     if (select.value === "plus") {
+                    let tableIncome = document.querySelector("#tableIncome tbody");
+        
+                    let newHead = document.createElement("th")
+                    let newRow = document.createElement("tr");
+                    let newCell1 = document.createElement("td");
+                    let newCell2 = document.createElement("td");
+        
+                    newCell1.textContent = description;
+                    newCell2.textContent = `$${price}`;
+        
+                    newRow.appendChild(newCell1);
+                    newRow.appendChild(newCell2);
+                    tableIncome.appendChild(newRow);
+                }
+        
+                if (select.value === "minus") {
+                    let tableOutgoing = document.querySelector("#tableOutgoing tbody");
+                    let description = document.querySelector("#type").value;
+        
+                    let newRow = document.createElement("tr");
+                    let newCell1 = document.createElement("td");
+                    let newCell2 = document.createElement("td");
+        
+                    newCell1.textContent = description;
+                    newCell2.textContent = `-$${price}`;
+        
+                    newRow.appendChild(newCell1);
+                    newRow.appendChild(newCell2);
+                    tableOutgoing.appendChild(newRow);
+                } */
--- storage/wkLocalStorage.js
+++ storage/wkLocalStorage.js
@@ -1,2 +1,98 @@
 /* Only for localstorage */
-let none = 0;
+let income = [];
+let outgoing = [];
+
+class Income {
+  constructor(description, price) {
+    this['description'] = description;
+    this['price'] = price;
+  }
+
+}
+
+class Outgoing {
+  constructor(description, price) {
+    this.description = description;
+    this.price = price;
+  }
+}
+
+self.onmessage = (e) => {
+  let select = e.data.select;
+  let description = e.data.description;
+
+  if (select === "load") {
+
+    income = e.data.data.income.map((incomee) => new Income(incomee.description, incomee.price));
+    outgoing = e.data.data.outgoing.map((outgoingg) => new Outgoing(outgoingg.description, outgoingg.price));
+
+    const Itemplate = income
+      .map(
+        (val, index) => `
+            <tr>
+              <td>${val.description}</td>
+              <td>${val.price}</td>
+            </tr>
+          `
+      )
+      .join("");
+
+    const Otemplate = outgoing
+      .map(
+        (val, index) => `
+            <tr>
+              <td>${val.description}</td>
+              <td>${val.price}</td>
+            </tr>
+          `
+      )
+      .join("");
+
+    // Enviamos las plantillas al hilo principal
+    self.postMessage({ select: "load", data: { Itemplate: Itemplate, Otemplate: Otemplate } });
+
+  }
+
+  if (select === "plus") {
+    let price = parseInt(e.data.price);
+    let newIncome = new Income(description, price);
+    income.unshift(newIncome);
+
+    // Creamos la plantilla
+    const Itemplate = income
+      .map(
+        (val, index) => `
+            <tr>
+              <td>${val.description}</td>
+              <td>${val.price}</td>
+            </tr>
+          `
+      )
+      .join("");
+
+    // Enviamos la plantilla al hilo principal
+    self.postMessage({ select: "incomee", data: income, template: Itemplate });
+
+  }
+   else if (select === "minus") {
+    let price = parseInt(e.data.price)
+    let newOutgoing = new Outgoing(description, price);
+    outgoing.unshift(newOutgoing);
+
+    // Creamos la plantilla
+    const Otemplate = outgoing
+      .map(
+        (val, index) => `
+            <tr>
+              <td>${val.description}</td>
+              <td>${val.price}</td>
+            </tr>
+          `
+      )
+      .join("");
+
+    // Enviamos la plantilla al hilo principal
+    self.postMessage({ select: "outgoingg", data: outgoing, template: Otemplate });
+  }
+
+};
