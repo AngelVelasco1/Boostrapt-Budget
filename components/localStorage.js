d90b8152275c8ec312ba4773b1a7068da3a0ae50
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-04-10 18:00:48 -0500

    feat: :sparkles: Implementacion boton eliminar

--- components/localStorage.js
+++ components/localStorage.js
@@ -62,35 +62,3 @@ export default {
         }
     }
 } 
-
-            /*     if (select.value === "plus") {
-                    let tableIncome = document.querySelector("#tableIncome tbody");
-        
-                    let newHead = document.createElement("th")
-                    let newRow = document.createElement("tr");
-                    let newCell1 = document.createElement("td");
-                    let newCell2 = document.createElement("td");
-        
-                    newCell1.textContent = description;
-                    newCell2.textContent = `$${price}`;
-        
-                    newRow.appendChild(newCell1);
-                    newRow.appendChild(newCell2);
-                    tableIncome.appendChild(newRow);
-                }
-        
-                if (select.value === "minus") {
-                    let tableOutgoing = document.querySelector("#tableOutgoing tbody");
-                    let description = document.querySelector("#type").value;
-        
-                    let newRow = document.createElement("tr");
-                    let newCell1 = document.createElement("td");
-                    let newCell2 = document.createElement("td");
-        
-                    newCell1.textContent = description;
-                    newCell2.textContent = `-$${price}`;
-        
-                    newRow.appendChild(newCell1);
-                    newRow.appendChild(newCell2);
-                    tableOutgoing.appendChild(newRow);
-                } */
--- storage/wkLocalStorage.js
+++ storage/wkLocalStorage.js
@@ -3,96 +3,102 @@ let income = [];
 let outgoing = [];
 
 class Income {
-  constructor(description, price) {
-    this['description'] = description;
-    this['price'] = price;
-  }
-
+    constructor(description, price) {
+        this['description'] = description;
+        this['price'] = price;
+    }
 }
-
 class Outgoing {
-  constructor(description, price) {
-    this.description = description;
-    this.price = price;
-  }
+    constructor(description, price) {
+        this.description = description;
+        this.price = price;
+    }
 }
 
 self.onmessage = (e) => {
-  let select = e.data.select;
-  let description = e.data.description;
 
-  if (select === "load") {
+    let select = e.data.select;
+    let description = e.data.description;
 
-    income = e.data.data.income.map((incomee) => new Income(incomee.description, incomee.price));
-    outgoing = e.data.data.outgoing.map((outgoingg) => new Outgoing(outgoingg.description, outgoingg.price));
+    if (select === "load") {
 
-    const Itemplate = income
-      .map(
-        (val, index) => `
+        income = e.data.data.income.map((incomee) => new Income(incomee.description, incomee.price));
+        outgoing = e.data.data.outgoing.map((outgoingg) => new Outgoing(outgoingg.description, outgoingg.price));
+
+        const Itemplate = income
+            .map(
+                (val, index) => `
             <tr>
               <td>${val.description}</td>
               <td>$ ${val.price}</td>
             </tr>
           `
-      )
-      .join("");
+            )
+            .join("");
 
-    const Otemplate = outgoing
-      .map(
-        (val, index) => `
-            <tr>
+        const Otemplate = outgoing
+            .map(
+                (val, index) => `
+            <tr class="rows">
               <td>${val.description}</td>
               <td>-$ ${val.price}</td>
+              <td><button class= "delete-button" data-index="${index}">Delete</button></td>
             </tr>
           `
-      )
-      .join("");
+            )
+            .join("");
+        self.postMessage({ select: "load", data: { Itemplate: Itemplate, Otemplate: Otemplate } });
+    }
 
-    // Enviamos las plantillas al hilo principal
-    self.postMessage({ select: "load", data: { Itemplate: Itemplate, Otemplate: Otemplate } });
+    if (select === "plus") {
+        let price = parseInt(e.data.price);
+        let newIncome = new Income(description, price);
+        income.unshift(newIncome);
 
-  }
-
-  if (select === "plus") {
-    let price = parseInt(e.data.price);
-    let newIncome = new Income(description, price);
-    income.unshift(newIncome);
-
-    // Creamos la plantilla
-    const Itemplate = income
-      .map(
-        (val, index) => `
+        const Itemplate = income
+            .map(
+                (val, index) => `
             <tr>
               <td>${val.description}</td>
               <td>${val.price}</td>
             </tr>
           `
-      )
-      .join("");
+            )
+            .join("");
 
-    // Enviamos la plantilla al hilo principal
-    self.postMessage({ select: "incomee", data: income, template: Itemplate });
+        self.postMessage({ select: "incomee", data: income, template: Itemplate });
 
-  }
-   else if (select === "minus") {
-    let price = parseInt(e.data.price)
-    let newOutgoing = new Outgoing(description, price);
-    outgoing.unshift(newOutgoing);
+    }
 
-    // Creamos la plantilla
-    const Otemplate = outgoing
-      .map(
-        (val, index) => `
-            <tr>
-              <td>${val.description}</td>
-              <td>${val.price}</td>
-            </tr>
-          `
-      )
-      .join("");
+    else if (select === "minus") {
+        let price = parseInt(e.data.price)
+        let newOutgoing = new Outgoing(description, price);
+        outgoing.unshift(newOutgoing);
+        const deleteButton = document.querySelectorAll(".delete-button");
+        deleteButton.forEach((button) => {
+            button.addEventListener("click", (e) => {
+                const index = e.target.getAtribute("data-index");
+                const row = document.getElementsByClassName(`rows${index}`);
+                row.parentNode.removeChild(row);
+                outgoing.splice(index, 1);
 
-    // Enviamos la plantilla al hilo principal
-    self.postMessage({ select: "outgoingg", data: outgoing, template: Otemplate });
-  }
+                const Otemplate = outgoing
+                    .map(
+                        (val, index) => `
+              <tr class="rows">
+                <td>${val.description}</td>
+                <td>${val.price}</td>
+                <td><button class= "delete-button" data-index="${index}">Delete</button></td>
+              </tr>
+            `
+                    )
+                    .join("");
+
+
+                self.postMessage({ select: "outgoingg", data: outgoing, template: Otemplate });
+            })
+        })
+
+    }
 
 };
